169772b98167a11847ce514e6d5b6e5b9ac519e9
Juan Carlos <Juan Carlos@Juanli>
2018-11-20 17:20:17 +0100

    Probando - JC

--- src/com/itt/arte/Pruebas.java
+++ src/com/itt/arte/Pruebas.java
@@ -5,7 +5,15 @@ public class Pruebas {
 	public static void main(String[] args) {
 //Hola desde mi casa by Miguel.
 // Hola desde tomillo, thuglife
-		System.out.println("Mi nombre es Pablo Emilio Escobar");
+		int x;
+		
+		x=(int)(Math.random()*20+1);
+		if (x>=15) {
+			System.out.println("Vas a cenar tortilla de patata");
+		}
+		else {
+			System.out.println("Hoy no cenas");
+		}
 	}
 
 }
