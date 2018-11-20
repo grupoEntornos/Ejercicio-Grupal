f016fa57edc29c8f1f6d52008a122eed7a2189cf
Juan Carlos <Juan Carlos@Juanli>
2018-11-20 17:12:02 +0100

    Prueba 2 Juan Carlos

--- src/com/itt/arte/Pruebas.java
+++ src/com/itt/arte/Pruebas.java
@@ -5,6 +5,7 @@ public class Pruebas {
 	public static void main(String[] args) {
 //Hola desde mi casa by Miguel.
 // Hola desde tomillo, thuglife
+		System.out.println("Mi nombre es Pablo Emilio Escobar");
 	}
 
 }
